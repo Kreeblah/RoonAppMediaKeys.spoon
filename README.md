a7003d5224f8fdecba1f41b60685a7cf662da001
Kreeblah <kreeblah@gmail.com>
2023-10-01 21:22:59 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # RoonAppMediaKeys.spoon
 Hammerspoon Spoon for sending play/pause/previous/next keys to Roon
+
+This is based on the Spoon to [send media key presses to Apple's Music.app](https://github.com/Hammerspoon/Spoons/tree/master/Source/MusicAppMediaFix.spoon), but modified to send them to [Roon](https://roon.app) instead.
+
+To use it, clone the repository into your Spoons directory:
+
+```
+git clone https://github.com/Kreeblah/RoonAppMediaKeys.spoon.git ~/.hammerspoon/Spoons/RoonAppMediaKeys.spoon
+```
+
+And then add the following to your `init.lua`:
+
+```
+-- Send play/pause/next/back to Roon
+hs.loadSpoon("RoonAppMediaKeys")
+spoon.RoonAppMediaKeys:start()
+```
---
+++ init.lua
@@ -0,0 +1,87 @@
+--- === RoonAppMediaKeys ===
+---
+--- Override macOS behaviour and send all media keys (play/prev/next) to Roon.app
+local obj = { __gc = true }
+--obj.__index = obj
+setmetatable(obj, obj)
+obj.__gc = function(t)
+    t:stop()
+end
+
+-- Metadata
+obj.name = "RoonAppMediaKeys"
+obj.version = "1.0"
+obj.author = "Matheus Salmi <mathsalmi@gmail.com>, Chris Jones <cmsj@tenshu.net>, Chris Gelatt <kreeblah@gmail.com>"
+obj.homepage = "https://github.com/Kreeblah/RoonAppMediaKeys.spoon"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+obj.eventtap = nil
+
+-- Internal function used to find our location, so we know where to load files from
+local function script_path()
+    local str = debug.getinfo(2, "S").source:sub(2)
+    return str:match("(.*/)")
+end
+obj.spoonPath = script_path()
+
+function obj:init()
+    self.eventtap = hs.eventtap.new({hs.eventtap.event.types.systemDefined}, self.mediaKeyCallback)
+end
+
+function obj.mediaKeyCallback(event)
+    local data = event:systemKey()
+    local roonApp = hs.application.applicationsForBundleID('com.roon.Roon')[1]
+
+    -- ignore everything but media keys
+    if data["key"] ~= "PLAY" and data["key"] ~= "FAST" and data["key"] ~= "REWIND" then
+        return false, nil
+    end
+
+    -- handle action
+    if data["down"] == false or data["repeat"] == true then
+        if data["key"] == "PLAY" then
+            hs.eventtap.keyStroke({}, "space", 0, roonApp)
+        elseif data["key"] == "FAST" then
+            hs.eventtap.keyStroke({"cmd"}, "k", 0, roonApp)
+        elseif data["key"] == "REWIND" then
+            hs.eventtap.keyStroke({"cmd"}, "j", 0, roonApp)
+        end
+    end
+
+    -- consume event
+    return true, nil
+end
+
+--- RoonAppMediaKeys:start()
+--- Method
+--- Starts the hs.eventtap that powers this Spoon
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The RoonAppMediaKeys object
+function obj:start()
+    if self.eventtap:isEnabled() ~= true then
+        self.eventtap:start()
+    end
+    return self
+end
+
+--- RoonAppMediaKeys:stop()
+--- Method
+--- Stops the hs.eventtap that powers this Spoon
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The RoonAppMediaKeys object
+function obj:stop()
+    if self.eventtap:isEnabled() then
+        self.eventtap:stop()
+    end
+    return self
+end
+
+return obj
